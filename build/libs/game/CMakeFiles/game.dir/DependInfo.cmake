2ef257d067d9969652544c9ab14861d44771536a
Musab-Farooq <masabfarooq0308@gmail.com>
2026-06-05 17:39:33 +0500

    glfw added

---
+++ build/libs/game/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/musab-farooq/Desktop/Break-Out/libs/game/src/BallObject.cpp" "libs/game/CMakeFiles/game.dir/src/BallObject.cpp.o" "gcc" "libs/game/CMakeFiles/game.dir/src/BallObject.cpp.o.d"
+  "/home/musab-farooq/Desktop/Break-Out/libs/game/src/game.cpp" "libs/game/CMakeFiles/game.dir/src/game.cpp.o" "gcc" "libs/game/CMakeFiles/game.dir/src/game.cpp.o.d"
+  "/home/musab-farooq/Desktop/Break-Out/libs/game/src/gamelevel.cpp" "libs/game/CMakeFiles/game.dir/src/gamelevel.cpp.o" "gcc" "libs/game/CMakeFiles/game.dir/src/gamelevel.cpp.o.d"
+  "/home/musab-farooq/Desktop/Break-Out/libs/game/src/gameobject.cpp" "libs/game/CMakeFiles/game.dir/src/gameobject.cpp.o" "gcc" "libs/game/CMakeFiles/game.dir/src/gameobject.cpp.o.d"
+  "/home/musab-farooq/Desktop/Break-Out/libs/game/src/window.cpp" "libs/game/CMakeFiles/game.dir/src/window.cpp.o" "gcc" "libs/game/CMakeFiles/game.dir/src/window.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/libs/graphics/CMakeFiles/graphics.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/musab-farooq/Desktop/Break-Out/libs/graphics/src/ParticleGenerator.cpp" "libs/graphics/CMakeFiles/graphics.dir/src/ParticleGenerator.cpp.o" "gcc" "libs/graphics/CMakeFiles/graphics.dir/src/ParticleGenerator.cpp.o.d"
+  "/home/musab-farooq/Desktop/Break-Out/libs/graphics/src/PostProcessor.cpp" "libs/graphics/CMakeFiles/graphics.dir/src/PostProcessor.cpp.o" "gcc" "libs/graphics/CMakeFiles/graphics.dir/src/PostProcessor.cpp.o.d"
+  "/home/musab-farooq/Desktop/Break-Out/libs/graphics/src/SpriteRenderer.cpp" "libs/graphics/CMakeFiles/graphics.dir/src/SpriteRenderer.cpp.o" "gcc" "libs/graphics/CMakeFiles/graphics.dir/src/SpriteRenderer.cpp.o.d"
+  "/home/musab-farooq/Desktop/Break-Out/libs/graphics/src/resource_manager.cpp" "libs/graphics/CMakeFiles/graphics.dir/src/resource_manager.cpp.o" "gcc" "libs/graphics/CMakeFiles/graphics.dir/src/resource_manager.cpp.o.d"
+  "/home/musab-farooq/Desktop/Break-Out/libs/graphics/src/shaders.cpp" "libs/graphics/CMakeFiles/graphics.dir/src/shaders.cpp.o" "gcc" "libs/graphics/CMakeFiles/graphics.dir/src/shaders.cpp.o.d"
+  "/home/musab-farooq/Desktop/Break-Out/libs/graphics/src/texture.cpp" "libs/graphics/CMakeFiles/graphics.dir/src/texture.cpp.o" "gcc" "libs/graphics/CMakeFiles/graphics.dir/src/texture.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
