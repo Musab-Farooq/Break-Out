e3191de8cfb1fc47d205bc713ddf90e919085c80
Musab-Farooq <masabfarooq0308@gmail.com>
2026-06-06 10:25:42 +0500

    glwf dependecy added

---
+++ build/libs/graphics/CMakeFiles/graphics.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graphics.dir/src/ParticleGenerator.cpp.o"
+  "CMakeFiles/graphics.dir/src/ParticleGenerator.cpp.o.d"
+  "CMakeFiles/graphics.dir/src/PostProcessor.cpp.o"
+  "CMakeFiles/graphics.dir/src/PostProcessor.cpp.o.d"
+  "CMakeFiles/graphics.dir/src/SpriteRenderer.cpp.o"
+  "CMakeFiles/graphics.dir/src/SpriteRenderer.cpp.o.d"
+  "CMakeFiles/graphics.dir/src/resource_manager.cpp.o"
+  "CMakeFiles/graphics.dir/src/resource_manager.cpp.o.d"
+  "CMakeFiles/graphics.dir/src/shaders.cpp.o"
+  "CMakeFiles/graphics.dir/src/shaders.cpp.o.d"
+  "CMakeFiles/graphics.dir/src/texture.cpp.o"
+  "CMakeFiles/graphics.dir/src/texture.cpp.o.d"
+  "libgraphics.a"
+  "libgraphics.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graphics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
